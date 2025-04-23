4c090688fcbf055cf03a075b700712b561c28d85
Yanis-Ourir <ourir.yanis@gmail.com>
2025-04-23 21:29:07 +0200

    added icon blade package

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -103,9 +103,9 @@
 
                             <div class="flex items-center justify-between mt-4 text-gray-400 text-sm border-t border-gray-800 pt-4">
                                 <div class="flex items-center gap-5">
-                                    {{-- <span class="flex items-center gap-1 hover:text-yellow-500"><x-icon-heart /></span> --}}
-                                    {{-- <span class="flex items-center gap-1 hover:text-yellow-500"><x-icon-comment /></span> --}}
-                                    {{-- <span class="flex items-center gap-1 hover:text-yellow-500"><x-icon-share /></span> --}}
+                                    <span class="flex items-center gap-1 hover:text-yellow-500"><x-icon-heart /></span>
+                                    <span class="flex items-center gap-1 hover:text-yellow-500"><x-icon-comment /></span>
+                                    <span class="flex items-center gap-1 hover:text-yellow-500"><x-icon-share /></span>
                                 </div>
                                 <span class="text-yellow-500 flex items-center gap-1 hover:underline">
                                     View on Twitter
@@ -117,7 +117,7 @@
             @empty
                 <div class="bg-[#1e1e1e] border border-gray-700 rounded-xl p-8 shadow-md shadow-black/20 text-center">
                     <div class="w-16 h-16 mx-auto mb-4 rounded-full bg-yellow-800/30 flex items-center justify-center">
-                        {{-- <x-icon-x /> --}}
+                        <x-icon-x />
                     </div>
                     <h2 class="text-xl font-semibold mb-2">Your feed is empty</h2>
                     <p class="text-gray-400">Select a category from the left sidebar to view posts</p>
