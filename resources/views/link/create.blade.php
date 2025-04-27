ad174b01684d5ce9404421b9b695802ba1723ab9
Yanis-Ourir <ourir.yanis@gmail.com>
2025-04-27 20:54:02 +0200

    updated links form

--- resources/views/link/create.blade.php
+++ resources/views/link/create.blade.php
@@ -7,26 +7,26 @@
 <div class="flex min-h-screen bg-[#161616] text-white">
   
 
-    <main class="flex-1 p-10 ml-64">
-        <div class="flex flex-col md:flex-row md:items-center justify-between gap-4 mb-8">
+    <main class="flex-1 p-10 mt-24">
+        <div class="flex flex-col md:flex-row md:items-center justify-center gap-4 mb-8">
             <h1 class="text-3xl font-bold text-white">Create your category</h1>
         </div>
 
-        <form action="{{ route('links.store') }}" method="POST" class="bg-[#1e1e1e] border border-gray-700 rounded-2xl p-6 shadow-md shadow-black/20">
+        <form action="{{ route('links.store') }}" method="POST" class="bg-[#1e1e1e] border border-gray-700 rounded-2xl p-6 shadow-md shadow-black/20 w-1/2 justify-center mx-auto">
             @csrf
             <div class="mb-4">
                 <label for="url" class="block text-sm font-medium text-gray-300">Url link</label>
-                <input type="text" name="url" id="name" required class="mt-1 block w-full bg-[#161616] border border-gray-700 rounded-lg shadow-sm focus:ring-yellow-500 focus:border-yellow-500 text-gray-300 placeholder-gray-500" placeholder="Enter category name">
+                <input type="text" name="url" id="name" required class="mt-1 p-2 block w-full bg-[#161616] border border-gray-700 rounded-lg shadow-sm focus:ring-yellow-500 focus:border-yellow-500 text-gray-300 placeholder-gray-500" placeholder="Enter category name">
             </div>
 
             <div class="mb-4">
                 <label for="application_name" class="block text-sm font-medium text-gray-300">Application Name</label>
-                <input type="text" name="application_name" id="name" required class="mt-1 block w-full bg-[#161616] border border-gray-700 rounded-lg shadow-sm focus:ring-yellow-500 focus:border-yellow-500 text-gray-300 placeholder-gray-500" placeholder="Enter category name">
+                <input type="text" name="application_name" id="name" required class="mt-1 p-2 block w-full bg-[#161616] border border-gray-700 rounded-lg shadow-sm focus:ring-yellow-500 focus:border-yellow-500 text-gray-300 placeholder-gray-500" placeholder="Enter category name">
             </div>
 
             <div class="mb-4">
                 <label for="category_id" class="block text-sm font-medium text-gray-300">Select Category</label>
-                <select name="category_id" id="category_id" required class="mt-1 block w-full bg-[#161616] border border-gray-700 rounded-lg shadow-sm focus:ring-yellow-500 focus:border-yellow-500 text-gray-300 placeholder-gray-500">
+                <select name="category_id" id="category_id" required class="mt-1 p-2 block w-full bg-[#161616] border border-gray-700 rounded-lg shadow-sm focus:ring-yellow-500 focus:border-yellow-500 text-gray-300 placeholder-gray-500">
                     @foreach ($categories as $category)
                         <option value="{{ $category->id }}">{{ $category->name }}</option>
                     @endforeach
